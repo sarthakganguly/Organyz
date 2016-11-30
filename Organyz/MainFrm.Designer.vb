35acb4c56e499e9afba791b2d68446200315be7a
sarthakganguly <sarthakgreaterindia@gmail.com>
2016-11-30 21:08:46 +0530

    First major commit

---
+++ Organyz/MainFrm.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainFrm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.folderPathTBox = New System.Windows.Forms.TextBox()
+        Me.setFolderPathBut = New System.Windows.Forms.Button()
+        Me.openFolderDlg = New System.Windows.Forms.FolderBrowserDialog()
+        Me.extractFilesBut = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'folderPathTBox
+        '
+        Me.folderPathTBox.Location = New System.Drawing.Point(102, 83)
+        Me.folderPathTBox.Name = "folderPathTBox"
+        Me.folderPathTBox.Size = New System.Drawing.Size(319, 20)
+        Me.folderPathTBox.TabIndex = 0
+        '
+        'setFolderPathBut
+        '
+        Me.setFolderPathBut.Location = New System.Drawing.Point(427, 81)
+        Me.setFolderPathBut.Name = "setFolderPathBut"
+        Me.setFolderPathBut.Size = New System.Drawing.Size(106, 22)
+        Me.setFolderPathBut.TabIndex = 1
+        Me.setFolderPathBut.Text = "Choose Directory"
+        Me.setFolderPathBut.UseVisualStyleBackColor = True
+        '
+        'extractFilesBut
+        '
+        Me.extractFilesBut.Location = New System.Drawing.Point(271, 152)
+        Me.extractFilesBut.Name = "extractFilesBut"
+        Me.extractFilesBut.Size = New System.Drawing.Size(75, 23)
+        Me.extractFilesBut.TabIndex = 2
+        Me.extractFilesBut.Text = "Extract File names"
+        Me.extractFilesBut.UseVisualStyleBackColor = True
+        '
+        'MainFrm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(616, 326)
+        Me.Controls.Add(Me.extractFilesBut)
+        Me.Controls.Add(Me.setFolderPathBut)
+        Me.Controls.Add(Me.folderPathTBox)
+        Me.Name = "MainFrm"
+        Me.Text = "Organyz"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents folderPathTBox As TextBox
+    Friend WithEvents setFolderPathBut As Button
+    Friend WithEvents openFolderDlg As FolderBrowserDialog
+    Friend WithEvents extractFilesBut As Button
+End Class
---
+++ Organyz/MainFrm.vb
@@ -0,0 +1,80 @@
+﻿Imports System.IO
+
+Public Class MainFrm
+
+    Private Function setInitFileExtensions()
+        Dim fileExtGlobal As New List(Of String)
+        fileExtGlobal.Add("txt")
+        fileExtGlobal.Add("mp4")
+        fileExtGlobal.Add("mp3")
+        fileExtGlobal.Add("jpg")
+        fileExtGlobal.Add("png")
+        fileExtGlobal.Add("xlsx")
+        fileExtGlobal.Add("pdf")
+        Return fileExtGlobal
+    End Function
+
+    Private Sub setFolderPathBut_Click(sender As Object, e As EventArgs) Handles setFolderPathBut.Click
+        Dim dlgResult = openFolderDlg.ShowDialog()
+        folderPathTBox.Text = openFolderDlg.SelectedPath
+    End Sub
+
+    Private Sub extractFiles(ByVal path As String)
+
+        ' get all file extensions to be extracted
+        Dim fileExtensions = setInitFileExtensions()
+
+        For Each fileType As String In fileExtensions
+            extractANDMoveFiles(path, fileType)
+        Next
+
+    End Sub
+
+    ' Sub proc actually doing the heavy lifting (moving) files
+    ' Call this from the Extract Files Button Click Event
+    Private Sub extractANDMoveFiles(ByVal path As String, ByVal extType As String)
+        Dim count As Short = 0
+
+        For Each file As String In Directory.GetFiles(path, ".")
+            Dim ext = retFileExtensions(file)
+
+            ' choose an extType collection instead of the raw ext type itself
+            ' for example, jpg, png etc should be stored in a Pictures Folder
+            ' p - created just to make things simple, a little extra space won't
+            ' matter much in the end :) It will get deallocated after this sub
+            If ext.Equals(extType) Then
+                Dim p = path + "\" + extType + "\" + file.Substring(file.LastIndexOf("\") + 1)
+                My.Computer.FileSystem.MoveFile(file, p)
+            End If
+
+        Next
+    End Sub
+
+    Private Function extToCategory(ByVal ext As String)
+        Dim catList As New List(Of String)
+        Dim catType As String = ""
+        Select Case ext
+            Case "txt" Or "md"
+                catType = "Text Files"
+            Case "png" Or "jpg" Or "bmp"
+                catType = "Images"
+            Case Else
+
+        End Select
+        Return catType
+    End Function
+
+    ' Extract Files Button Click Handler
+    Private Sub extractFilesBut_Click(sender As Object, e As EventArgs) Handles extractFilesBut.Click
+        extractFiles(folderPathTBox.Text)
+    End Sub
+
+    ' Required to get the file extension of the file
+    ' to be moved
+    Private Function retFileExtensions(ByVal filename As String)
+        Dim ext As String = ""
+        ext = filename.Substring(filename.LastIndexOf(".") + 1)
+        Return ext
+    End Function
+
+End Class
